9d3025dcbd664bf530fba8278283a71eb070abe8
Yoshimasa Niwa <niw@niw.at>
2014-09-06 18:49:32 +0900

    Reduce some redundancies.

--- src/main/scala/WordCountJob.scala
+++ src/main/scala/WordCountJob.scala
@@ -2,10 +2,10 @@ import com.twitter.scalding._
 
 class WordCountJob(args: Args) extends Job(args) {
   TextLine(args("in"))
-      .flatMap('line -> 'word) { line: String => tokenize(line) }
+      .flatMap('line -> 'word) { tokenize }
       .groupBy('word) { _.size }
       .write(Tsv(args("out")))
 
-  def tokenize(text: String): Array[String] =
+  def tokenize(text: String) =
     text.toLowerCase.replaceAll("[^a-zA-Z0-9\\s]", "").split("\\s+")
 }
